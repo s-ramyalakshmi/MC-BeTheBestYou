07eabde8aede22251c20400aae6f06e4bd72cc04
Sukruthi Anilkumar <sanilku2@asu.edu>
2023-12-03 22:01:50 -0700

    Navigation Bar

---
+++ app/src/main/java/com/example/bethebestyou/NavigationBar.java
@@ -0,0 +1,44 @@
+package com.example.bethebestyou;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class NavigationBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_bar);
+
+        Button btnVitalsPage = findViewById(R.id.btnVitalsPage);
+        Button btnMentalHealthPage = findViewById(R.id.btnMentalHealthPage);
+        Button btnPersonalGoalPage = findViewById(R.id.btnPersonalGoalPage);
+        Button btnResultsProgress = findViewById(R.id.btnResultsProgress);
+
+
+
+        btnMentalHealthPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start MentalHealthActivity when btnMentalHealthPage is clicked
+                Intent intent = new Intent(NavigationBar.this, MentalHealthActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnPersonalGoalPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start PersonalGoalActivity when btnPersonalGoalPage is clicked
+                Intent intent = new Intent(NavigationBar.this, PersonalGoalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/bethebestyou/PersonalGoalActivity.java
@@ -0,0 +1,4 @@
+package com.example.bethebestyou;
+
+public class PersonalGoalActivity {
+}
